c8e66ab0846739a0b7bfe0ef0b71636cc666b3e2
acp-proju <32454409+acp-proju@users.noreply.github.com>
2018-02-24 01:12:11 +0200

    Updates

---
+++ plantasia2/Assets/scripts/ClickerControlScript.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickerControlScript : MonoBehaviour {
+
+    public Button harvestButton, SellButton, ExitButton;
+
+    public GameObject ShopPanel,HarvestPanel, SalesPanel, UpgradePanel;
+
+    public Text BeansText, MoneyText, BeansStext, MoneyStext;
+
+    public static double MoneyAmount = 100; // how much money collected 
+
+    private double d;
+
+    public double BeansAmount; //how meany beans harvested 
+    public double BeanPrice; // how much money per bean sold
+    public double HarvestS; // how many beans harvested per click / or per second with automatic harvest
+    public double SoldS; // how many beans sold per click / or per second with automatic sell
+
+
+	void Start () {
+        
+        SellButton.interactable = true;
+        
+        ShopPanel.gameObject.SetActive(false);
+        SalesPanel.gameObject.SetActive(false);
+        UpgradePanel.gameObject.SetActive(false);
+        InvokeRepeating("BeansPerSec", 1.0f, 0.1f);
+        InvokeRepeating("MoneyPerSec", 1.0f, 0.1f);
+
+
+    }
+	
+
+	void Update () {
+
+        HarvestS = UpgradeButtonDisplay.HarvestS;
+        SoldS = UpgradeButtonDisplay.SoldS;
+        BeanPrice = UpgradeButtonDisplay.BeanPrice;
+        
+        EnoughBeansToSell();
+
+
+        BeansStext.text = "Net:" + ((HarvestS - 1) - (SoldS - 1));
+        MoneyStext.text = "Cps:" + ((SoldS -1)* BeanPrice);
+        BeansText.text = "Beans:" + BeansAmount.ToString("F0");
+        MoneyAmountDisplay();
+        //MoneyText.text = "$" + MoneyAmount.ToString("F0");
+
+    }
+
+    public void IncreaseBeansAmount()
+    {
+       
+        BeansAmount = BeansAmount + HarvestS;
+    }
+
+    public void IncreaseMoneyAmount()
+    {
+        
+        BeansAmount -= 1;
+        MoneyAmount = MoneyAmount + BeanPrice * SoldS;
+    }
+
+
+    void EnoughBeansToSell()
+    {
+        if (BeansAmount <SoldS)
+            SellButton.interactable = false;
+        if (BeansAmount >= SoldS)
+            SellButton.interactable = true;
+           
+    }
+
+    public void OpenShop()
+    {
+        ShopPanel.gameObject.SetActive (true);
+        ExitButton.interactable = true;
+        ExitButton.gameObject.SetActive(true);
+    }
+
+    public void CloseShop()
+    {
+        ShopPanel.gameObject.SetActive (false);
+    }
+
+
+    public void BeansPerSec()
+    {
+        if ((SoldS - 1) <= BeansAmount)
+            BeansAmount = BeansAmount + (((HarvestS - 1)) * 0.1f) - ((SoldS - 1) *0.1f);
+
+    }
+    public void MoneyPerSec()
+    {
+        if((SoldS -1) <= BeansAmount)
+            MoneyAmount = MoneyAmount + ((SoldS -1) * BeanPrice * 0.1f);
+            
+       
+        if((SoldS -1) > BeansAmount)
+            MoneyAmount = MoneyAmount + ((HarvestS -1) * BeanPrice * 0.1f);
+        
+    }
+
+    void MoneyAmountDisplay()
+    {
+
+        if (MoneyAmount <= 1000000000000000000)
+        {
+            d = (MoneyAmount / 1000000000000000000);
+             MoneyText.text = "$" +  d.ToString("F3") + "Qt";
+        }
+        if (MoneyAmount >= 1000000000000000 && MoneyAmount < 1000000000000000000)
+        {
+            d = (MoneyAmount / 1000000000000000);
+             MoneyText.text = "$" +  d.ToString("F3") + "Q";
+        }
+        if (MoneyAmount >= 1000000000000 && MoneyAmount < 1000000000000000)
+        {
+            d = (MoneyAmount / 1000000000000);
+            MoneyText.text = "$" + d.ToString("F3") + "T";
+        }
+        if (MoneyAmount >= 1000000000 && MoneyAmount < 1000000000000)
+        {
+            d = (MoneyAmount / 1000000000);
+            MoneyText.text = "$" + d.ToString("F3") + "B";
+        }
+        if (MoneyAmount >= 1000000 && MoneyAmount < 1000000000)
+        {
+            d = (MoneyAmount / 1000000);
+            MoneyText.text = "$" + d.ToString("F3") + "M";
+        }
+        if (MoneyAmount < 1000000)
+            MoneyText.text = "$" + MoneyAmount.ToString("F0");
+
+
+    }
+
+    public void doExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ plantasia2/Assets/scripts/UpgradeButtonDisplay.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class UpgradeButtonDisplay : MonoBehaviour {
+
+
+    public Upgrade upgrade;
+
+    public float AmountOwned;
+
+    public static int HarvestS = 1;
+    public static int BeanPrice = 1; 
+    public static int SoldS = 1;
+    public int b;
+    public double a;
+    public double UpgradePrice;
+    public double c;
+    public Text NameText;
+    public Text DescriptionText;
+    public Text PriceText;
+
+    public Button button;
+
+	void Start () {
+        NameText.text = upgrade.name;
+        DescriptionText.text = upgrade.description;
+        UpgradePrice = upgrade.price;
+        AmountOwned = upgrade.AmountOwned;
+        
+
+
+
+    }
+
+    void Update()
+    {
+        
+        EnoughMoneyToBuy();
+        PriceRounder();
+        //PriceText.text = UpgradePrice.ToString();
+        if (AmountOwned + 1 > 10)
+            b = 2;
+        if (AmountOwned + 1 < 10)
+            b = 1;
+
+    }
+
+    public void OnClicked()
+    {
+        
+        HarvestS = HarvestS + upgrade.HarvestUpgrade;
+        BeanPrice = BeanPrice + upgrade.PriceUpgrade;
+        SoldS = SoldS + upgrade.SoldUpgrade;
+
+        ClickerControlScript.MoneyAmount -= UpgradePrice;
+        
+        UpgradePrice = (AmountOwned + 1) * upgrade.price * (AmountOwned + 1) * b; 
+
+        AmountOwned += 1;
+
+
+
+
+        Debug.Log(HarvestS);
+        Debug.Log(upgrade.HarvestUpgrade);
+        Debug.Log(ClickerControlScript.MoneyAmount);
+        Debug.Log(UpgradePrice);
+
+    }
+
+    void EnoughMoneyToBuy()
+    {
+        if (ClickerControlScript.MoneyAmount < UpgradePrice)
+            button.interactable = false;
+        if (ClickerControlScript.MoneyAmount >= UpgradePrice)
+            button.interactable = true;
+
+    }
+
+    void PriceRounder()
+    {
+
+        if (UpgradePrice <= 1000000000000000000)
+        {
+            c = (UpgradePrice / 1000000000000000000);
+            PriceText.text = "$" + c.ToString() + "Qt";
+        }
+        if (UpgradePrice >= 1000000000000000 && UpgradePrice < 1000000000000000000)
+        {
+            c = (UpgradePrice / 1000000000000000);
+            PriceText.text = "$" + c.ToString() + "Q";
+        }
+        if (UpgradePrice >= 1000000000000 && UpgradePrice < 1000000000000000)
+        {
+            c = (UpgradePrice / 1000000000000);
+            PriceText.text = "$" + c.ToString() + "T";
+        }
+        if (UpgradePrice >= 1000000000 && UpgradePrice < 1000000000000)
+        {
+            c = (UpgradePrice / 1000000000);
+            PriceText.text = "$" + c.ToString() + "B";
+        }
+        if (UpgradePrice >= 1000000 && UpgradePrice < 1000000000)
+        {
+            c = (UpgradePrice / 1000000);
+            PriceText.text = "$" + c.ToString() + "M";
+        }
+        if (UpgradePrice < 1000000)
+            PriceText.text = "$" + UpgradePrice.ToString();
+
+
+    }
+}
